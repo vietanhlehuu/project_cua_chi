7c2733c2750e7ad1a5686dc0ced1ef2f524baf85
anhle <anh@silentium.io>
2021-01-05 22:11:18 +0700

    next

--- server.js
+++ server.js
@@ -1,10 +1,34 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const path = require("path");
+const bodyParser = require("body-parser");
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.use(bodyParser.json());
+app.set("view engine", "ejs");
+// Public tat ca cac file trong thu muc views
+app.use("/public", express.static(path.join(__dirname, "views")));
+
+// Quan tam tu dong lenh nay tro xuong
+app.get("/", function (req, res) {
+  res.render("ph");
 });
+
+app.get(
+  "/place1",
+  function (req, res, next) {
+    // xac thuc nguoi dung
+    const condition = true;
+    if (condition) {
+      next();
+    } else {
+      res.redirect("/");
+    }
+  },
+  function (req, res) {
+    res.render("place1");
+  }
+);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
